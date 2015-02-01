653fe5e63c5e68db16c9792b83e9476bff3f042a
Hristo Stoichev <hrist0.stoichev@mail.bg>
2015-02-01 16:59:26 +0200

    CoffeeMachine

---
+++ C#1/ExamPreparation/CoffeeMachine/CoffeeMachine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class CoffeeMachine
+{
+    static void Main()
+    {
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int n3 = int.Parse(Console.ReadLine());
+        int n4 = int.Parse(Console.ReadLine());
+        int n5 = int.Parse(Console.ReadLine());
+        float a = float.Parse(Console.ReadLine());
+        float p = float.Parse(Console.ReadLine());
+        float moneyInTheMachine = (float)((n1 * 0.05) + (n2 * 0.10) + (n3 * 0.20) + (n4 * 0.50) + n5);
+        float change = (float)a - p;
+        if (a >= p && moneyInTheMachine >= change)
+        {
+            Console.WriteLine("Yes {0:f2}", moneyInTheMachine - change);
+        }
+        else if (p > a)
+        {
+            Console.WriteLine("More {0:f2}", p - a);
+        }
+        else if (a >= p && moneyInTheMachine < change)
+        {
+            Console.WriteLine("No {0:f2}", change - moneyInTheMachine);
+        }
+    }
+}
