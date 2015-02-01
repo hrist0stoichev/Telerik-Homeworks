d200cb02741faa1e91aee3eeece4f402868bf444
Hristo Stoichev <hrist0.stoichev@mail.bg>
2015-02-01 18:15:34 +0200

    DrunkenNumbers

---
+++ C#1/ExamPreparation/DrunkenNumbers/DrunkenNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrunkenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputRows = int.Parse(Console.ReadLine());
+            var vladoBeers = 0;
+            var mitkoBeers = 0;
+
+            for (int i = 0; i < inputRows; i++)
+            {
+                var number = Console.ReadLine().TrimStart('-').TrimStart('0');
+                for (int j = 0; j < number.Length; j++)
+                {
+                    if (j < number.Length / (float)2)
+                    {
+                        if (j + 0.5 == number.Length / (float)2)
+                        {
+                            vladoBeers += number[j] - '0';
+                        }
+
+                        mitkoBeers += number[j] - '0';
+                    }
+                    else
+                    {
+                        vladoBeers += number[j] - '0';
+                    }
+                }
+            }
+
+            Console.WriteLine(vladoBeers == mitkoBeers ? "No " + vladoBeers * 2 : vladoBeers > mitkoBeers ? "V " + (vladoBeers - mitkoBeers) : "M " + (mitkoBeers - vladoBeers));
+        }
+    }
+}
